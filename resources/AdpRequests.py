24d3deb2f7555f12c1e72845f87c4acfdb017d0f
Fredy Santamaria <fsantamaria@berryit.net>
2023-04-13 15:41:28 -0400

    Added the ApiConnector class

--- resources/AdpRequests.py
+++ resources/AdpRequests.py
@@ -1,3 +1,9 @@
+import requests
+import json
+import logging
+import time
+
+
 class UrlGenerator:
     """
        Helper class for generating URLs for ADP API calls.
@@ -74,3 +80,138 @@ class UrlGenerator:
             skip += 25
             processed += 25
         return list_of_time_card_urls
+
+
+class ApiConnector:
+    """
+        Class for connecting to the ADP API and retrieving data.
+    """
+
+    def __init__(self, full_certificate: tuple[str, str], base_64_credentials: str):
+        """Initializes the class with the provided certificate.
+
+        Args:
+            full_certificate (tuple[str, str]): The certificate to use for the API requests.
+            base_64_credentials (str): The base64 encoded client id and secret for authentication.
+        """
+        self.certificate = full_certificate
+        self.payload = {}
+        self.files = {}
+        self.token = None
+        self.token_expiration = None
+        self.base64_credentials = base_64_credentials
+        self.token_api_endpoint = "https://accounts.adp.com/auth/oauth/v2/token?grant_type=client_credentials"
+        self.api_host = "api.adp.com"
+
+    def get_token(self):
+        """
+        Retrieves: a bearer token from the specified URL and returns it.
+
+        Raises:
+            requests.exceptions.RequestException: If an error occurs while making the request.
+
+        """
+
+        try:
+            headers = {
+                'Authorization': f'Basic {self.base64_credentials}'
+            }
+            response = requests.request("POST",
+                                        url=self.token_api_endpoint,
+                                        headers=headers,
+                                        data=self.payload,
+                                        files=self.files,
+                                        cert=self.certificate)
+
+            # Parse the response to get the token
+            bearer_token_response = json.loads(response.text)
+            access_token = bearer_token_response.get('access_token')
+            bearer_token = f"Bearer {access_token}"
+            # Set the token and the expiration time (60 minutes)
+            self.token = bearer_token
+            self.token_expiration = time.time() + 3600
+
+        except requests.exceptions.RequestException as e:
+            logging.error(f"An error occurred while retrieving the token from {self.token_api_endpoint}: {e}")
+            raise
+
+    def verify_token(self):
+        if self.token is None or self.token_expiration is None or time.time() > self.token_expiration:
+            self.get_token()
+
+    def get_time_cards(self, max_number_of_time_cards, main_associate_id, start_date):
+        self.verify_token()
+        list_of_urls = UrlGenerator().generate_timecard_api_urls(max_number_of_time_cards,
+                                                                 start_date,
+                                                                 main_associate_id)
+        list_of_time_cards = []
+        headers = {
+            'Authorization': self.token,
+            'Accept-Encoding': 'gzip, deflate',
+            'Host': self.api_host,
+            'Connection': 'Keep-Alive',
+            'User-Agent': 'Apache-HttpClient/4.5.2(Java/1.8.0_112)',
+            'Accept': 'application/json',
+            'Cookie': 'BIGipServerp_dc1_mobile_sor_integratedezlm=3938124043.15395.0000; BIGipServerp_dc2_mobile_apache_sor=3013608203.5377.0000; BIGipServerp_mkplproxy-dc1=1633878283.20480.0000; BIGipServerp_mkplproxy-dc2=670892811.20480.0000; BIGipServerp_dc1_mobile_apache_sor=153042955.5377.0000; Cookie_1=value'
+        }
+        for time_card_url in list_of_urls:
+            with requests.request(
+                    "GET",
+                    url=time_card_url,
+                    headers=headers,
+                    data=self.payload,
+                    cert=self.certificate
+            ) as response:
+
+                if response.status_code != 200:
+                    logging.error(
+                        f"An error occurred while retrieving time cards from {time_card_url}. "
+                        f"Response code: {response.status_code}")
+                    raise
+                else:
+                    response_text = json.loads(response.text)
+                    complete_indicator = response_text.get("meta").get("completeIndicator")
+                    if complete_indicator:
+                        return list_of_time_cards
+                    list_of_time_cards.append(response_text)
+
+    def get_employees(self, max_number_of_employees):
+        """
+                Retrieves a list of employees from the ADP API.
+
+                Args:
+                    max_number_of_employees (int): The maximum number of employees to retrieve.
+
+                Returns:
+                    list:  A list of employees in JSON format.
+                """
+
+        self.verify_token()
+
+        try:
+            list_of_urls = UrlGenerator().generate_employee_api_urls_(500)
+            list_of_employees = []
+            headers = {
+                'Authorization': self.token,
+                'Cookie': 'BIGipServerp_dc1_mobile_apache_sor=706691083.5377.0000; BIGipServerp_mkplproxy-dc1=2803368203.20480.0000; BIGipServerp_mkplproxy-dc2=654115595.20480.0000'
+            }
+            for employee_url in list_of_urls:
+                with requests.request(
+                        "GET",
+                        url=employee_url,
+                        headers=headers,
+                        data=self.payload,
+                        cert=self.certificate
+                ) as response:
+
+                    if response.status_code == 204:
+                        break
+                    else:
+                        response_text = json.loads(response.text)
+                        list_of_employees.append(response_text)
+            return list_of_employees
+
+        except json.decoder.JSONDecodeError as e:
+            logging.error(
+                f"An error occurred while retrieving employees.\n Error: {e}")
+            raise
