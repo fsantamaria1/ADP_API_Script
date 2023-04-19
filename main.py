e91355d76108e738609535d9923746e8e82193c9
Fredy Santamaria <fsantamaria@berryit.net>
2023-04-19 10:08:24 -0400

    Replaced credentials.py with a .env file

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from resources.adp_requests import APIConnector
 import base64
-import time
 from resources.database import get_engine, get_session, Base
-from resources import credentials
+from dotenv import load_dotenv
+import os
 
 
 def create_tables():
@@ -20,15 +20,20 @@ def insert_data():
 # TODO: Work on the create_table and insert_data functions
 
 if __name__ == '__main__':
+    # Load environment variables
+    load_dotenv()
+
     # Encode credentials and convert to base 64
-    byte_credentials = credentials.client_id_secret.encode('ascii')
+    client_id_secret = f"{os.environ.get('client_id')}:{os.environ.get('client_secret')}"
+    byte_credentials = client_id_secret.encode('ascii')
     base64_byte_credentials = base64.b64encode(byte_credentials)
     base64_credentials = base64_byte_credentials.decode('ascii')
 
     # Create APIConnector class object to get all the necessary responses
-    connector = APIConnector(credentials.certificate, base64_credentials)
+    certificate = (os.environ.get('cert_file_path'), os.environ.get('key_file_path'))
+    connector = APIConnector(certificate, base64_credentials)
 
     # Get some API responses
     employees = connector.get_employees("number_of_employees")
 
-    time_cards = connector.get_time_cards("number_of_time_cards", credentials.main_associate_id, "YYYY-MM-DD")
+    time_cards = connector.get_time_cards("number_of_time_cards", os.environ.get('main_associate_id'), "YYYY-MM-DD")
